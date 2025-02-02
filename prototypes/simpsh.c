1a8a5f349e82d49ad92d444189aba0c4e934c761
chepeniv <jose.olmos@atlasschool.com>
2025-02-02 04:21:30 -0600

    first draft of simpsh.c prototype

---
+++ prototypes/simpsh.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+char **tokenize(char *inputline)
+{
+	int max_tokens = 2024;
+	char *delims = " \n";
+	char **tokens = malloc(sizeof(void *) * max_tokens);
+	char *tok = strtok(inputline, delims);
+
+	int i = 0;
+	do {
+		tokens[i] = tok;
+		tok = strtok(NULL, delims);
+	} while ((++i < max_tokens) && (tok != NULL));
+
+	tokens[i] = NULL;
+
+	free(tok);
+	return (tokens);
+}
+
+void print_str_array(char **strings)
+{
+	for (int i = 0; strings[i] != NULL; i++)
+		printf("%d: %s\n", i, strings[i]);
+}
+
+int main(void)
+{
+	/*
+	 * fork, execve, wait
+	 *    pid checks
+	 */
+
+	char *inputline = NULL;
+	char **input_tokens = NULL;
+	size_t input_len = 0;
+
+	do {
+		if (inputline != NULL)
+		{
+			if (!strcmp(inputline, "exit\n"))
+				break;
+			else
+			{
+				input_tokens = tokenize(inputline);
+				print_str_array(input_tokens);
+				free(input_tokens);
+			}
+		}
+		printf("$ ");
+	} while (getline(&inputline, &input_len, stdin));
+
+	free(inputline);
+
+	return (0);
+}
