68e9c8b3a461d1b4cb8668b532f68b2198fb79a1
“internashionalist” <taylor.thames@atlasschool.com>
2025-02-03 12:24:24 -0600

    draft of files - some psuedo and notes in separators.c and redirections.c so far

---
+++ redirections.c
@@ -0,0 +1,15 @@
+/*
+handling >, >>, <, <<, etc:
+- parse input tokens, looking for redirects
+- get following file name
+- remove the tokens from that array
+- in child process
+  - open the file witih whatever flags
+  - redirect the I/O (dup2???)
+  - close up file descriptors
+  - unless I missed something, execve
+
+
+- probably need _strcmp for this
+- cases could include multiple redirects, no file name, invalid file name, whatever
+*/
