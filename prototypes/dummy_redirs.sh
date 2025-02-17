1a3f4a98827818ca880bf6360434d4bfe3c62458
chepeniv <jose.olmos@atlasschool.com>
2025-02-16 22:46:38 -0600

    standardized the output filenames

--- prototypes/dummy_redirs.sh
+++ prototypes/dummy_redirs.sh
@@ -1,4 +1,4 @@
-cat -n _which.c >> _which.copy > _which.copy2 && ls -al .. > ls1 >> ls2 && ls >> _ls.out ; ls > _ls2.out & pwd > _pwd.out
+cat -n _which.c >> ./output/which1.out > ./output/which2.out > ./output/which3.out && ls -al .. > ./output/ls1.out >> ./output/ls2.out && ls >> ./output/ls3.out ; ls > ./output/ls4.out & pwd > ./output/pwd1.out
 # cat -n _which.c > _which.copy # && ls -l -a .. > _ls.out| echo hello world! > hw.out # || semi << LL ; end > R < L >> RR
 # twoAmp >> RR > R && bar | semi ; twoBar || amp  > R & end < L
 # pwd < L >> RR ;  git status -s  --show-stash || echo hello again! # | sed A B C >> RR  << LL  |  end > R << LL
