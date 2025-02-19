d37f7d961216cd5ae281b015913fb03b97dd2231
chepeniv <jose.olmos@atlasschool.com>
2025-02-19 14:52:49 -0600

    extended the redir-append prototype to test stdout restoration

--- prototypes/redir-append.exr.c
+++ prototypes/redir-append.exr.c
@@ -5,33 +5,35 @@
 #include <errno.h>
 #include <sys/stat.h> /* file mode_t defs */
 
-
-/*
- * create/open filename given and redirect stdout to it in append mode
- */
+/* create/open filename given and redirect stdout to it in append mode */
 
 int main(int c, char **args)
 {
 	int fdesc;
+	int hold_stdout;
 	int fmode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP; /* rw-rw---- */
-	int fflags = O_CREAT | O_APPEND | O_WRONLY;
+	int fflags = O_CREAT | /*O_APPEND |*/ O_RDWR;
 
 	if (c < 2)
-		return (EINVAL);
+		return (1);
+
+	hold_stdout = dup(STDOUT_FILENO);
 
 	fdesc = open(args[1], fflags, fmode);
+	dup2(fdesc, STDOUT_FILENO);
+	/* close(fdesc); */
 
-	if (fdesc < 0)
-		return (errno);
+	printf("file: output redirected to new file descriptor\n");
+	printf("file: testing subsequent writes to stdout\n");
 
-	if (dup2(fdesc, STDOUT_FILENO) < 0)
-		return(errno);
 
-	printf("if successful, a new file with this should've been created\n");
-	printf("testing what happens in subsequent calls to stdout\n");
-	printf("testing...\n");
+	dup2(hold_stdout, STDOUT_FILENO);
+	/* close(hold_stdout); */
 
-	close(fdesc);
+	printf("stdout: file descriptor closed\n");
+	printf("stdout: output restored to stdout\n");
+
+
 
 	return (0);
 }
